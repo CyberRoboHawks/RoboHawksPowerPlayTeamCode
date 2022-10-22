ad1ccec86240ed8d83317d118427eaa0c840978c
laupetre <laupetre@yahoo.com>
2022-10-22 18:06:19 -0400

    update

---
+++ AutonomousCommands.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Log;
+
+public class AutonomousCommands extends Commands{
+
+    public void runCommands(AutonomousBase.StartingSide side){
+
+    }
+}
