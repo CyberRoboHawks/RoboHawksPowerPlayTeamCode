1ecdb1c673527ce6d629c7dced68ce47d45a8c76
laupetre <laupetre@yahoo.com>
2022-10-22 18:18:14 -0400

    add PowerPlay enums

---
+++ PowerPlayEnums.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PowerPlayEnums {
+    public enum liftPosition {
+        Floor,
+        Drive,
+        LowJunction,
+        MidJunction,
+        HighJunction
+    }
+
+    public enum parkingZone {
+        Zone1Bolt,
+        Zone2Bulb,
+        Zone3Panel
+    }
+
+    public enum startingSide {
+        Left,
+        Right
+    }
+
+    public enum strafeDirection {
+        Right,
+        Left
+    }
+}
